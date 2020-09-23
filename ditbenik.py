bd5da7005b10ced68f0f47eeb79aa8f755686930
unknown <31924@ma-web.nl>
2020-09-24 00:16:46 +0200

    mijn keuze verhaal

--- ditbenik.py
+++ ditbenik.py
@@ -22,17 +22,6 @@ if antwoord == "B" or antwoord == "b":
 if antwoord == "C" or antwoord == "c":
     print("Correct!")
 
-print("Heb ik ervaring in programmeren?")
-print("A:Nee")
-print("B:ja")
-print("Kies uit A of B")
-antwoord = input()
-
-if antwoord == "A" or antwoord == "a":
-   	print("Correct!")
-if antwoord == "B" or antwoord == "b":
-	print("Wrong!")
-
 print("Hoe oud ben ik?")
 print("A:17")
 print("B:16")
@@ -45,6 +34,45 @@ if antwoord == "B" or antwoord == "b":
 	print("Correct!")	
 if antwoord == "C" or antwoord == "c":
 	print("Wrong!")
+
+print("let's play a game. The game is gonna go about, can you surive a zombie apocalypse?")
+print("a single zombie is running towards you choose a weapon to kill the zombie")
+print("A:Gun")
+print("B:Chopper")
+print("C:knife")
+antwoord = input()
+
+if antwoord == "A" or antwoord == "a":
+	print("Nice you killed the zombie")
+if antwoord == "B" or antwoord == "b":
+	print("Nice job you killed the zombie")
+if antwoord == "C" or antwoord == "c":
+	print("Nice work you killed the zombie")
+
+
+print("After you killed the zombie you made much noise, the other zombies are gonna look for you so choose something wou wanna go away with")
+print("A:go get the motorcycle in your garage")
+print("B:use the car across the street")
+print("C:get the bike next to you")
+antwoord = input()
+
+if antwoord == "A" or antwoord == "a":
+	print("Good choice")
+if antwoord == "B" or antwoord == "b":
+	print("Safe choice")
+if antwoord == "C" or antwoord == "c":
+	print("risky choice")
+
+print("after 20 minutes driving your front tire has gone flat, if you walk 5 km to the north you will arrive at a parking lot where you can find a new vehicle. After 2 km you hear screams you see a group of people being chased by zombies what are you going to do?")
+print("A:chose a rocket launcher and shoot your shot at the zombies")
+print("B:chose a M4A1 and try to kill them")
+antwoord = input()
+
+if antwoord == "A" or antwoord == "a":
+	print("Nice work you killed the zombies and met new people, one of them knew where a militare base was where everybody would be safe from the zombies. after 4 hours you guys reached the base and your safe now.")
+if antwoord == "B" or antwoord == "b":
+	print("you are to late the bullets didn't do much damage the people got invected and now they are chasing you, you got caught and you became a zombie GAME OVER")
+
 input()
 
 
