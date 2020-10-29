e8eaf14b3793aca902809346dace52b4d7553fa8
unknown <31924@ma-web.nl>
2020-10-29 11:50:52 +0100

    python les-05

---
+++ PYTb1L5 shuffle and return.py
@@ -0,0 +1,25 @@
+import random
+
+original = "hallo"
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
+print("echte woord = hallo")
+print("_________________________________________________________")
+
+import random
+
+original = "zarkozarko"
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
+print("echte woord = zarkozarko")
+
+
+print("__________________________________________________________")
+
+import random
+
+original = "python"
+randomised = ''.join(random.sample(original, len(original)))
+print(randomised)
+
+print("echte woord = Python")
